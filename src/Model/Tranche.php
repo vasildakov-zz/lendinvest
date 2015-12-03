dd3407a377ff17df03a67e6026403c8325c09034
Vasil Dakov <vasildakov@gmail.com>
2015-12-03 11:11:33 +0000

    Refactored Tranche class, green

--- src/Model/Tranche.php
+++ src/Model/Tranche.php
@@ -37,4 +37,54 @@ class Tranche implements EntityInterface
     {
         $this->createdAt = new \DateTime('now');
     }
+
+    /**
+     * @param float $maxAmount
+     */
+    public function setMaxAmount($maxAmount)
+    {
+        $this->maxAmount = $maxAmount;
+
+        return $this;
+    }
+
+
+    /**
+     * @return float $maxAmount
+     */
+    public function getMaxAmount()
+    {
+        return $this->maxAmount;
+    }
+
+
+    public function getCurrentAmount()
+    {
+        $amount = 0;
+
+        if (!empty($this->investments)) {
+            foreach ($this->investments as $investment) {
+                $amount += $investment->getAmount();
+            }
+        }
+        return $amount;
+    }
+
+
+    public function hasInvestments()
+    {
+        return !empty($this->investments);
+    }
+
+
+    public function getInvestments()
+    {
+        return $this->investments;
+    }
+
+
+    public function addInvestment(Investment $investment)
+    {
+        $this->investments[] = $investment;
+    }
 }
