e831b44fc369aeae2e7ce560b1d0cd6bc25aaffa
Vasil Dakov <vasildakov@gmail.com>
2015-12-03 10:49:17 +0000

    Implement Wallet deposit and withdraw money, red

--- tests/ModelTest/WalletTest.php
+++ tests/ModelTest/WalletTest.php
@@ -20,4 +20,48 @@ class WalletTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertClassHasAttribute('balance', 'LendInvest\Model\Wallet');
     }
+
+
+    public function testInitialWalletBalanceIsZero()
+    {
+        $wallet = new Wallet;
+        $this->assertEquals(0, $wallet->getBalance());
+    }
+
+
+    public function testCanDepositMoney()
+    {
+        $amount = 1000;
+
+        $wallet = new Wallet;
+        $wallet->depositMoney($amount);
+        $this->assertEquals($amount, $wallet->getBalance());
+    }
+
+
+    public function testCanWithdrawMoney()
+    {
+        $amount = 1000;
+
+        $wallet = new Wallet;
+        $wallet->depositMoney($amount);
+        $this->assertEquals($amount, $wallet->getBalance());
+
+        $wallet->withdrawMoney(300);
+        $this->assertEquals(700, $wallet->getBalance());
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testWithdrawMoneyThrowsAnExpeptionOnNegativeBalance()
+    {
+        $amount = 300;
+
+        $wallet = new Wallet;
+        $wallet->depositMoney($amount);
+        $this->assertEquals($amount, $wallet->getBalance());
+
+        $wallet->withdrawMoney(500);
+    }
 }
