7dce53418349185f0af36e6b21f3a092bc80e367
Vasil Dakov <vasildakov@gmail.com>
2015-12-03 11:44:07 +0000

    Refactored Tranche, Implemented interest

--- src/Model/Tranche.php
+++ src/Model/Tranche.php
@@ -3,19 +3,25 @@ namespace LendInvest\Model;
 
 use LendInvest\Model\Loan;
 use LendInvest\Model\Investment;
+use LendInvest\Model\Interest;
 
 /**
  * Tranche
  *
- * @package Lendinvest
+ * @package LendInvest
  * @author Vasil Dakov <vasildakov@gmail.com>
  */
 class Tranche implements EntityInterface
 {
     /**
-     * @var \Lendinvest\Model\Loan $loan
+     * @var \LendInvest\Model\Loan $loan
      */
     private $loan;
+
+    /**
+     * @var \LendInvest\Model\Interest $interest
+     */
+    private $interest;
 
     /**
      * @var float $maxAmount
@@ -31,6 +37,8 @@ class Tranche implements EntityInterface
      * @var \DateTime $createdAt
      */
     private $createdAt;
+
+
 
 
     public function __construct()
@@ -86,5 +94,25 @@ class Tranche implements EntityInterface
     public function addInvestment(Investment $investment)
     {
         $this->investments[] = $investment;
+    }
+
+
+    /**
+     * @param \LendInvest\Model\Interest $interest
+     */
+    public function setInterest($interest)
+    {
+        $this->interest = $interest;
+
+        return $this;
+    }
+
+
+    /**
+     * @return \LendInvest\Model\Interest $interest
+     */
+    public function getInterest()
+    {
+        return $this->interest;
     }
 }
