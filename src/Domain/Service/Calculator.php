e81d2ed6c7b591cb7fca457f78987dd0eba62a80
Vasil Dakov <vasildakov@gmail.com>
2016-12-07 23:37:22 +0000

    Updated calculator

--- src/Domain/Service/Calculator.php
+++ src/Domain/Service/Calculator.php
@@ -12,6 +12,10 @@ class Calulator
 {
     public function __invoke($amount, $interest)
     {
+        // 3 / 31 = 0.0967741935
+        // 29 * 0.0967741935 = 2.8064516129
+        // 1000 * 2.8064 / 100 = 28.064
+
         return 1000 * 2.8064 / 100;
     }
 }
